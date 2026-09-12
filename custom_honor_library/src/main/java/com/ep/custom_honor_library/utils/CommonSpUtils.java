bddb66024d64b2ff224874cf36d2289e5fed8b23
new_ttt <bkm8363@yeah.net>
2026-09-12 11:46:03 +0800

    星梦手绘馆 荣耀B包

--- custom_honor_library/src/main/java/com/ep/custom_honor_library/utils/CommonSpUtils.java
+++ custom_honor_library/src/main/java/com/ep/custom_honor_library/utils/CommonSpUtils.java
@@ -15,7 +15,7 @@ public class CommonSpUtils {
     public static String APP_USER_STATUS = "app_user_status";
 
     public static String SP_IS_FIRST_APP_STR = "sp_first_start_app";
-    public static String SP_OAID_STR = "sp_oaid_str";
+
     public static String SP_ANDROID_ID_STR = "sp_android_id_str";
     public static String SP_CHANNEL_NUM_STR = "sp_channel_str";
 
